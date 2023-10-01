30ef54bacb9942930c45157f55332fc8ed850fbe
AreebaTariq768 <raoareeba2000@gmail.com>
2023-10-01 07:10:56 -0700

    feat -Added Meals component and setup api

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import {BsHandThumbsUp} from 'react-icons/bs';
 import {useGlobalContext} from '../context';
 
 const Meals = () => {
-  const context = useGlobalContext()
-  return <h1>Meals Component</h1>
+  const { meals } = useGlobalContext();
+
+  return  <section className="section-center">
+  {meals.map((singleMeal) => {
+    const { idMeal, strMeal: title, strMealThumb: image } = singleMeal
+    return <article key={idMeal} className="single-meal" >
+      <img src={image} className="img" />
+      <footer>
+        <h5>{title}</h5>
+        <button className='like-btn'><BsHandThumbsUp/></button>
+      </footer>
+    </article>
+  })}
+</section>
+
+
+  
+ 
+
 }
 
 export default Meals
--- src/context.js
+++ src/context.js
@@ -1,26 +1,36 @@
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useEffect ,useState} from 'react'
+import axios from 'axios'
+
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
 const AppContext = React.createContext();
 
+
 const AppProvider = ({children}) =>{
+  const [meals, setMeals] = useState([]);
+
+  const fetchMeals = async (url) => {
     
+    try {
+      const {data} = await axios.get(url)
+      setMeals(data.meals)
+   
+    }
+    catch (e) {
+      console.log(e.response)
+    }
+    
+  }
     useEffect(()=>{
-        const fetchData = async() =>{
-          try {
-            const response = await fetch('https://randomuser.me/api/')
-            const data = await response.json();
-            console.log(data)
-          } catch (error) {
-                    console.log(error)
-          }
-        }
-        fetchData()
+       
+      fetchMeals(allMealsUrl)
       },[])
 
 
     return (
     <AppContext.Provider
-    value={"hello"}>
+    value={{meals}}>
     {children}
     </AppContext.Provider>
 
